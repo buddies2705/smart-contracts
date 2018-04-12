c12b870674769b87729c9a9c761d97b379d03fff
gaurav <buddies2705@gmail.com>
2018-04-12 15:29:35 +0530

    smoke tests

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.2;
+
+contract Election{
+		//Store candidate
+		//read candidate	
+	 //consructor
+
+	 string public candidate;
+
+	 function Election() public {
+	 	candidate = "Candidate 1";
+	 }
+}
